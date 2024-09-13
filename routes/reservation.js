b0e6dc939521c10df0fbd6d00aeeb4106d0e20e4
kjha4951 <kjha4951@gmail.com>
2024-09-13 08:50:15 +0530

    add vercel

--- routes/reservation.js
+++ routes/reservation.js
@@ -3,7 +3,9 @@ const router=express.Router();
 const sendreservation=require('../controller/resevation');
 
 router.post('/send',sendreservation)
-
+router.get('/',(req,res)=>{
+   res.send('hello');
+})
 
 
 module.exports=router;
